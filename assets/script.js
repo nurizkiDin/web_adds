356da89df5a04ed4faac755199386e7a219fb72b
Nurizki <nutrisi1udin@gmail.com>
2022-11-07 07:09:36 +0700

    perbaruan ke tptm towing

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,5 @@
 function chatWa() {
-    window.open('https://api.whatsapp.com/send?phone=6281318418028');
-}
-
-var sliderIndex = 0;
-var slideIndex = 1;
-showSlider(slideIndex);
-
-function plusSlider(n) {
-    showSlider(slideIndex += n);
-}
-function showSlider(n) {
-    var i;
-    var x = document.getElementsByClassName("img-slider");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.opacity = "0";
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.opacity = "1";
-    x[slideIndex-1].style.display = "block";
+    window.open('https://api.whatsapp.com/send?phone=6281297918007');
 }
 function menuHp(x) {
     let menu = document.getElementById("navhp");
@@ -32,17 +12,48 @@ function menuHp(x) {
     }
     x.classList.toggle("change");
 }
-// carousel();
-// function carousel() {
-//     let x = document.getElementsByClassName("img-slider");
-//     for (let i = 0; i < x.length; i++) {
-//         x[i].style.opacity = "0";
-//         x[i].style.display = "none";
-//     }
-//     sliderIndex++;
-//     if (sliderIndex > x.length) {sliderIndex = 1}
-//     x[sliderIndex-1].style.opacity = "1";
-//     x[sliderIndex-1].style.display = "block";
-//     setTimeout(carousel, 5000);
-//     return sliderIndex;
-// }
+function slider() {
+    var imgs = document.querySelectorAll('.img-slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
+    
+    let manual = function(index) {
+        imgs.forEach((img) => {
+            img.classList.remove('active');
+        });
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+
+        imgs[index].classList.add('active');
+        btns[index].classList.add('active');
+    };
+    btns.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            manual(i);
+            currentSlide = i;
+        });
+    });
+
+    let auto = () => {
+        let aktif = document.getElementsByClassName('active');
+        setTimeout(function() {
+            [...aktif].forEach((slideAktif) => {
+                slideAktif.classList.remove('active');
+            });
+            imgs[currentSlide].classList.add('active');
+            btns[currentSlide].classList.add('active');
+            currentSlide++;
+
+            if (imgs.length == currentSlide) {
+                currentSlide = 0;
+            }
+            if (currentSlide >= imgs.length) {
+                return;
+            }
+            auto();
+        }, 5000);
+    };
+    auto();
+}
+slider();
